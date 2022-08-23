e2aa111de2b2058912abf6ead6d05dfe927401bb
No-Hex <evilheroe1@gmail.com>
2022-08-23 19:56:41 -0400

    added pagination

--- frontend/src/Pages/AdminArticles/AdminArticles.jsx
+++ frontend/src/Pages/AdminArticles/AdminArticles.jsx
@@ -2,17 +2,57 @@ import { useEffect, useState } from "react"
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Layout from '../../Components/Layout/Layout';
-import { image } from '../../Constants/index';
+import ReactPaginate from 'react-paginate';
 
 function AdminArticles() {
-
-    const [articles, setArticles] = useState([])
 
     useEffect(() => {
         axios.get("http://127.0.0.1:8000/api/v1/articles").then((response) => {
             setArticles(response.data);
         })
     }, [])
+
+
+    const [articles, setArticles] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const articlesPerPage = 6
+    const pagesVisited = pageNumber * articlesPerPage
+
+    const displayArticles = articles.slice(pagesVisited, pagesVisited + articlesPerPage).map(article => {
+        return (
+
+            <div className="flex justify-between">
+                <div className="flex space-x-10 items-center">
+                    <div className="">
+                        <img src={article.image_url} className='w-60 rounded-lg object-cover' alt="" />
+                    </div>
+
+                    <div className="space-y-8">
+                        <h3 className='text-gray-200 font-bold uppercase'>{article.titulo}</h3>
+                        <p className='text-gray-400 font-semibold uppercase'>00/00/00</p>
+                        <div className="">
+                            <Link to={`/admin/articles/new_post/${article.titulo}`} className='hover:text-gray-900 text-gray-200 hover:bg-gray-200 px-8 py-1 border-2 rounded ease-in duration-300'>Add Post</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-center">
+                    <div className='flex flex-col justify-center gap-8'>
+                        <div className=""><button className='text-gray-400 hover:text-gray-200 ease-in duration-300 text-3xl'><span class="iconify" data-icon="carbon:view"></span></button></div>
+                        <div className=""><Link to={`/admin/articles/update_article/${article.id}/${article.titulo}`} className='text-gray-400 hover:text-gray-200 ease-in duration-300 text-3xl'><span class="iconify" data-icon="fluent:edit-20-regular"></span></Link></div>
+                        <div className=""><button className='text-gray-400 hover:text-gray-200 ease-in duration-300 text-3xl'><span class="iconify" data-icon="ion:trash-bin-outline"></span></button></div>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
+    const pageCount = Math.ceil(articles.length / articlesPerPage);
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    };
+
 
     return (
         <Layout>
@@ -34,35 +74,20 @@ function AdminArticles() {
                 <hr className="text-gray-200" />
 
                 <div className="mt-5 grid grid-cols-2 gap-8">
-                    {articles.map((article) => {
-                        return (
+                    {displayArticles}
 
-                            <div className="flex justify-between">
-                                <div className="flex space-x-10 items-center">
-                                    <div className="">
-                                        <img src={article.image_url} className='w-60 rounded-lg object-cover' alt="" />
-                                    </div>
-
-
-                                    <div className="space-y-8">
-                                        <h3 className='text-gray-200 font-bold uppercase'>{article.titulo}</h3>
-                                        <p className='text-gray-400 font-semibold uppercase'>00/00/00</p>
-                                        <div className="">
-                                            <Link to={`/admin/articles/new_post/${article.titulo}`} className='hover:text-gray-900 text-gray-200 hover:bg-gray-200 px-8 py-1 border-2 rounded ease-in duration-300'>Add Post</Link>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="flex justify-center">
-                                    <div className='items-center space-y-5'>
-                                        <div className=""><button className='text-gray-400 hover:text-gray-200 ease-in duration-300 text-3xl'><span class="iconify" data-icon="carbon:view"></span></button></div>
-                                        <div className=""><Link to={`/admin/articles/update_article/${article.titulo}`} className='text-gray-400 hover:text-gray-200 ease-in duration-300 text-3xl'><span class="iconify" data-icon="fluent:edit-20-regular"></span></Link></div>
-                                        <div className=""><button className='text-gray-400 hover:text-gray-200 ease-in duration-300 text-3xl'><span class="iconify" data-icon="ion:trash-bin-outline"></span></button></div>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
+                    <ReactPaginate
+                        previousLabel={"Back"}
+                        nextLabel={"Next"}
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        containerClassName={"paginationBtns flex w-full justify-between"}
+                        pageLinkClassName={"text-gray-200"}
+                        previousLinkClassName={"previousBtn text-gray-200"}
+                        nextLinkClassName={"nextBtn text-gray-200"}
+                        disabledClassName={"paginationDisabled"}
+                        activeClassName={"paginationActive"}
+                    />
                 </div >
             </div >
         </Layout>
